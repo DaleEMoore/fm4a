e5dbf7ec22868763587b68c783f95bd5fd2150f0
Dale E. Moore <DaleEMoore@gMail.Com>
2014-08-21 13:11:25 -0500

    Initial commit

---
+++ fileMaker.py
@@ -0,0 +1,13 @@
+# fileMaker.py - read File Maker .FMP12 and interpret it on Android.
+
+__author__ = 'dalem'
+
+# TODO; Open .FMP12 file.
+# TODO; Figure out what's inside the .FMP12 file.
+#       od -cb JSON.FMP12
+#       to see ASCII characters and Octal otherwise.
+
+#       Build the code that will read and interpret .FMP12 files.
+
+
+
